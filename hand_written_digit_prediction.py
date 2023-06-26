166978b69baea8ef91eceb5f8341c2950141ab45
SRIHARI <137819588+srihari16028@users.noreply.github.com>
2023-06-27 05:01:59 +0530

    Add files via upload

---
+++ hand_written_digit_prediction.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load and preprocess the MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(-1, 28, 28, 1) / 255.0
+x_test = x_test.reshape(-1, 28, 28, 1) / 255.0
+
+# Create the CNN model
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+# Compile and train the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=2, batch_size=32, validation_data=(x_test, y_test))
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print("Test accuracy:", test_acc)
+
+# Display x_test images and corresponding predicted outputs
+n_images = 5  # Number of images to display
+
+fig, axes = plt.subplots(1, n_images, figsize=(n_images * 2, 2))
+
+for i in range(n_images):
+    image = x_test[i].reshape(28, 28)
+    prediction = np.argmax(model.predict(np.expand_dims(x_test[i], axis=0)))
+
+    axes[i].imshow(image, cmap='gray')
+    axes[i].set_title(f'Predicted: {prediction}')
+    axes[i].axis('off')
+
+plt.tight_layout()
+plt.show()
